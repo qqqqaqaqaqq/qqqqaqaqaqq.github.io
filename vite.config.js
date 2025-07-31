8a83066e1740d97b1cd0e88e4f153f19fb81e395
Hong <jooimtn@naver.com>
2025-07-31 12:52:13 +0900

    base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/qqqqaqaqaqq.github.io/",
+    base: "/",
     plugins: [
         react(),
         compression({ algorithm: "gzip" }),
